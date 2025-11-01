98f67bc94e3d4169245984d00fbaf2c53f7024ed
Ojax <ojalrak@gmail.com>
2025-11-01 01:16:45 +0000

    375

---
+++ Public-Key Crptography/EulersTotient.py
@@ -0,0 +1,7 @@
+p = 857504083339712752489993810777
+q = 1029224947942998075080348647219
+N = p * q
+#euliers totient function
+#(N)=(p−1)(q−1)
+pn = (p - 1) * (q - 1)
+print(pn)  # euliers totient function value for N
